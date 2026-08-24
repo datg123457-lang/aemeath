24fd01b4b51d23018228e00a531e6a909c4061bd
nhuthien2404 <0025413904@student.dthu.edu.vn>
2026-08-24 16:42:04 +0700

    new

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
-l
 int main() {
     cout << "Nguyen SI Cuong \n";
-    cout <<" 2D Game Demo - OOP C++ \n";
+    cout <<"2D Game Demo - OOP C++ \n";
     return 0;
 }
